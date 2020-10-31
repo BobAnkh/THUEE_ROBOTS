19e0e51f5d835283d010de5e53292bc7c3952ac7
BobAnkh <bobankhshen@gmail.com>
2020-10-31 11:31:36 +0800

    fix(path): select only pictures to process

--- exp2/svm_hog.py
+++ exp2/svm_hog.py
@@ -4,7 +4,7 @@
 # @Author       : BobAnkh
 # @Github       : https://github.com/BobAnkh
 # @Date         : 2020-10-22 19:29:56
-# @LastEditTime : 2020-10-24 13:32:59
+# @LastEditTime : 2020-10-31 11:30:17
 # @Description  :
 # @Copyright 2020 BobAnkh
 
@@ -14,10 +14,21 @@ from skimage.feature import hog
 import os
 import random
 import numpy as np
-import json
+import re
 # from tqdm import tqdm
 from sklearn.externals import joblib
 import logging
+
+
+def read_data(path):
+    img = cv2.imread(path)
+    img = cv2.resize(img, (60, 80))  # average size
+    fd = hog(img,
+            orientations=9,
+            pixels_per_cell=(5, 5),
+            cells_per_block=(2, 2),
+            multichannel=True)
+    return fd
 
 
 def SVM_HOG_TRAIN(DATA_TRAIN, model_place='exp2.model', loglevel='DEBUG'):
@@ -27,13 +38,15 @@ def SVM_HOG_TRAIN(DATA_TRAIN, model_place='exp2.model', loglevel='DEBUG'):
     Args:
         DATA_TRAIN (str): 训练集地址.
         model_place (str, optional): 模型存储的位置. Defaults to 'exp2.model'.
-        loglevel (str, optional): log输出的级别,'DEBUG'即全输出,'NOTSET'即无输出. Defaults to 'DEBUG'.
+        loglevel (str, optional): log输出的级别,'DEBUG'即全输出,'INFO'即无输出. Defaults to 'DEBUG'.
     '''
     if loglevel == 'DEBUG':
-        logging.basicConfig(format="[%(levelname)s]%(message)s", level=logging.DEBUG)
+        logging.basicConfig(format="[%(levelname)s]%(message)s",
+                            level=logging.DEBUG)
     else:
-        logging.basicConfig(format="[%(levelname)s]%(message)s", level=logging.NOTSET)
-    
+        logging.basicConfig(format="[%(levelname)s]%(message)s",
+                            level=logging.INFO)
+
     logging.debug('-----------Train start!-----------')
     train_data = []
     categories = os.listdir(DATA_TRAIN)
@@ -44,14 +57,12 @@ def SVM_HOG_TRAIN(DATA_TRAIN, model_place='exp2.model', loglevel='DEBUG'):
         load_cat = 'loading category: ' + category
         logging.debug(load_cat)
         for file in os.listdir(path):
-            img = cv2.imread(os.path.join(path, file))
-            img = cv2.resize(img, (60, 80))  # average size
-            fd = hog(img,
-                     orientations=9,
-                     pixels_per_cell=(5, 5),
-                     cells_per_block=(2, 2),
-                     multichannel=True)
-            train_data.append((fd, category))
+            img_path = os.path.join(path, file)
+            if re.match(r'.*\.(jpg|jpeg|png)$', img_path):
+                fd = read_data(img_path)
+                train_data.append((fd, category))
+            else:
+                continue
     # 随机调整数据顺序
     random.shuffle(train_data)
     logging.debug('read data success!')
@@ -60,7 +71,8 @@ def SVM_HOG_TRAIN(DATA_TRAIN, model_place='exp2.model', loglevel='DEBUG'):
     n = int(0.8 * len(train_data))
     train_set = train_data[:n]
     val_set = train_data[n:]
-    TV_number = 'Train_set: ' + str(len(train_set)) + ' Val_set: ' + str(len(val_set))
+    TV_number = 'Train_set: ' + str(len(train_set)) + ' Val_set: ' + str(
+        len(val_set))
     logging.debug(TV_number)
 
     # unzip dataset
@@ -100,15 +112,17 @@ def SVM_HOG_TEST(DATA_TEST, model_place='exp2.model', loglevel='DEBUG'):
     Args:
         DATA_TEST (str): 测试集地址.
         model_place (str, optional): 模型存储的位置. Defaults to 'exp2.model'.
-        loglevel (str, optional): log输出的级别,'DEBUG'即全输出,'NOTSET'即无输出. Defaults to 'DEBUG'.
+        loglevel (str, optional): log输出的级别,'DEBUG'即全输出,'INFO'即无输出. Defaults to 'DEBUG'.
 
     Returns:
         dict: 字典结构(json)的测试图片及其类别名称.
     '''
     if loglevel == 'DEBUG':
-        logging.basicConfig(format="[%(levelname)s]%(message)s", level=logging.DEBUG)
+        logging.basicConfig(format="[%(levelname)s]%(message)s",
+                            level=logging.DEBUG)
     else:
-        logging.basicConfig(format="[%(levelname)s]%(message)s", level=logging.NOTSET)
+        logging.basicConfig(format="[%(levelname)s]%(message)s",
+                            level=logging.INFO)
     logging.debug('-----------Test start!-----------')
     # Load model
     classifier = joblib.load(model_place)
@@ -118,15 +132,12 @@ def SVM_HOG_TEST(DATA_TEST, model_place='exp2.model', loglevel='DEBUG'):
     test_imgs = os.listdir(DATA_TEST)
     logging.debug('loading test images')
     for test_img in test_imgs:
-        path = os.path.join(DATA_TEST, test_img)
-        img = cv2.imread(path)
-        img = cv2.resize(img, (60, 80))  # average size
-        fd = hog(img,
-                 orientations=9,
-                 pixels_per_cell=(5, 5),
-                 cells_per_block=(2, 2),
-                 multichannel=True)
-        test_data.append(fd)
+        img_path = os.path.join(DATA_TEST, test_img)
+        if re.match(r'.*\.(jpg|jpeg|png)$', img_path):
+            fd = read_data(img_path)
+            test_data.append(fd)
+        else:
+            continue
     # test
     logging.debug('Test result:')
     test_predict = classifier.predict(test_data)
@@ -141,8 +152,8 @@ def SVM_HOG_TEST(DATA_TEST, model_place='exp2.model', loglevel='DEBUG'):
 
 
 if __name__ == '__main__':
-    DATA_TRAIN = 'data/train'
-    DATA_TEST = 'data/test'
-    SVM_HOG_TRAIN(DATA_TRAIN)
-    test_result = SVM_HOG_TEST(DATA_TEST)
+    DATA_TRAIN = os.path.join('exp2', 'data', 'train')  # path: exp2/data/train
+    DATA_TEST = os.path.join('exp2', 'data', 'test')    # path: exp2/data/test
+    SVM_HOG_TRAIN(DATA_TRAIN, loglevel='DEBUG')
+    test_result = SVM_HOG_TEST(DATA_TEST, loglevel='DEBUG')
     print(test_result)
