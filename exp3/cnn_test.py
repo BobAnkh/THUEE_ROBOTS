5995ad98fa56f7033fc7c734ed285b7eff55e79b
BobAnkh <bobankhshen@gmail.com>
2020-11-28 17:29:36 +0800

    feat(exp3): add reduced cnn
    - refactor some codes to be well-organized
    - reduce the size of input with new net structure
    - remove unused codes
    - update a new pretrained model

--- exp3/cnn_test.py
+++ exp3/cnn_test.py
@@ -9,66 +9,31 @@ from torchvision import transforms
 np.set_printoptions(threshold=np.inf)
 
 
-class AlexNet(nn.Module):
+class RCNet(nn.Module):
     def __init__(self, num_classes=6):
-        super(AlexNet, self).__init__()
+        super(RCNet, self).__init__()
         self.features = nn.Sequential(
-            nn.Conv2d(3, 32, kernel_size=11, stride=4, padding=0),
+            nn.Conv2d(3, 32, kernel_size=3, stride=2),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=3, stride=1),
+            nn.Conv2d(32, 32, kernel_size=3),
             nn.ReLU(inplace=True),
             nn.MaxPool2d(kernel_size=3, stride=2),
-            nn.Conv2d(32, 32, kernel_size=5, padding=0),
-            nn.ReLU(inplace=True),
-            nn.MaxPool2d(kernel_size=3, stride=2),
-            nn.Conv2d(32, 32, kernel_size=3, padding=0),
-            nn.ReLU(inplace=True),
-            nn.MaxPool2d(kernel_size=4, stride=3),
         )
         self.classifier = nn.Sequential(
             nn.Dropout(),
-            # nn.Linear(32 * 4 * 4, 32),
-            # nn.ReLU(inplace=True),
-            # nn.Dropout(),
-            nn.Linear(32 * 2 * 2, num_classes),
+            nn.Linear(32 * 3 * 3, num_classes),
         )
 
     def forward(self, x):
         x = self.features(x)
-        x = x.view(x.size(0), 32 * 2 * 2)
+        x = x.view(x.size(0), 32 * 3 * 3)
         x = self.classifier(x)
         return x
 
 
-model = torch.load('cnn_wts_reduce.pth')
-
-# save parameters in npy
-# params = {}  #change the tpye of 'generator' into dict
-# for name, param in model.named_parameters():
-#     params[name] = param.detach().cpu().numpy()
-# # with open('param_reduce/alexnet_param.txt', 'w') as f:
-# for key in params.keys():
-#     np.save('param_reduce/' + key + '.npy', params[key])
-
-test_dir = 'Datasets/multiple_animals/test'
-test_data = os.listdir(test_dir)
-
-data_transforms = transforms.Compose([
-    transforms.Resize((299, 299)),
-    transforms.CenterCrop(224),
-    transforms.ToTensor()
-])
-data_normalize = transforms.Normalize([0.44087456, 0.39025736, 0.43862119],
-                                      [0.18242574, 0.19140723, 0.18536106])
-# img=Image.open('1.jpg')
-# box=img.crop((200,130,440,360))
-# re = box.resize((120,120),Image.ANTIALIAS)
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-test_label = [
-    'batman', 'jienigui', 'miaowazhongzi', 'pikachu', 'saquirrel',
-    'xiaohuolong'
-]
-
-
-def test_alexnet(model, _transforms, root_dir, data_list):
+def test_rcnet(model, _transforms, root_dir, data_list, data_normalize,
+               test_label, device):
     result = {}
     model.eval()
     for data_dir in data_list:
@@ -78,10 +43,42 @@ def test_alexnet(model, _transforms, root_dir, data_list):
         inputs = inputs.to(device)
         outputs = model(inputs)
         _, preds = torch.max(outputs, 1)
-        np.save(data_dir + '.npy', outputs.cpu().detach().numpy())
         result[data_dir] = test_label[preds[0]]
     return result
 
 
-result = test_alexnet(model, data_transforms, test_dir, test_data)
-print(result)
+def main():
+    # load model
+    model = torch.load('cnn_wts_reduce.pth')
+
+    # save parameters in npy
+    params = {}
+    for name, param in model.named_parameters():
+        params[name] = param.detach().cpu().numpy()
+
+    for key in params.keys():
+        np.save('param/' + key + '.npy', params[key])
+
+    test_dir = 'Datasets/multiple_animals/test'
+    test_data = os.listdir(test_dir)
+
+    data_transforms = transforms.Compose([
+        transforms.CenterCrop(300),
+        transforms.Resize((24, 24)),
+        transforms.ToTensor()
+    ])
+    data_normalize = transforms.Normalize([0.44087456, 0.39025736, 0.43862119],
+                                          [0.18242574, 0.19140723, 0.18536106])
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    test_label = [
+        'batman', 'jienigui', 'miaowazhongzi', 'pikachu', 'saquirrel',
+        'xiaohuolong'
+    ]
+
+    result = test_rcnet(model, data_transforms, test_dir, test_data,
+                        data_normalize, test_label, device)
+    print(result)
+
+
+if __name__ == '__main__':
+    main()
--- exp3/cnn_train.py
+++ exp3/cnn_train.py
@@ -6,67 +6,27 @@ from torch import nn
 from torchvision import datasets, transforms
 
 
-class AlexNet(nn.Module):
+class RCNet(nn.Module):
     def __init__(self, num_classes=6):
-        super(AlexNet, self).__init__()
+        super(RCNet, self).__init__()
         self.features = nn.Sequential(
-            nn.Conv2d(3, 32, kernel_size=11, stride=4, padding=0),
+            nn.Conv2d(3, 32, kernel_size=3, stride=2),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=3, stride=1),
+            nn.Conv2d(32, 32, kernel_size=3),
             nn.ReLU(inplace=True),
             nn.MaxPool2d(kernel_size=3, stride=2),
-            nn.Conv2d(32, 32, kernel_size=5, padding=0),
-            nn.ReLU(inplace=True),
-            nn.MaxPool2d(kernel_size=3, stride=2),
-            nn.Conv2d(32, 32, kernel_size=3, padding=0),
-            nn.ReLU(inplace=True),
-            nn.MaxPool2d(kernel_size=4, stride=3),
         )
         self.classifier = nn.Sequential(
             nn.Dropout(),
-            # nn.Linear(32 * 4 * 4, 32),
-            # nn.ReLU(inplace=True),
-            # nn.Dropout(),
-            nn.Linear(32 * 2 * 2, num_classes),
+            nn.Linear(32 * 3 * 3, num_classes),
         )
 
     def forward(self, x):
         x = self.features(x)
-        x = x.view(x.size(0), 32 * 2 * 2)
+        x = x.view(x.size(0), 32 * 3 * 3)
         x = self.classifier(x)
         return x
-
-
-batch_size = 256
-
-data_transforms = transforms.Compose([
-    transforms.Resize((299, 299)),
-    transforms.CenterCrop(224),
-    transforms.ToTensor(),
-    transforms.Normalize([0.44087456, 0.39025736, 0.43862119],
-                         [0.18242574, 0.19140723, 0.18536106])
-])
-train_dir = 'Datasets/multiple_animals/train'
-
-train_datasets = datasets.ImageFolder(train_dir, data_transforms)
-print("Dataset len:", len(train_datasets))
-train_db, val_db = torch.utils.data.random_split(train_datasets, [
-    round(len(train_datasets) * 0.8),
-    (len(train_datasets) - round(len(train_datasets) * 0.8))
-])
-image_datasets = {'train': train_db, 'val': val_db}
-
-dataloaders = {
-    x: torch.utils.data.DataLoader(image_datasets[x],
-                                   batch_size=batch_size,
-                                   shuffle=True,
-                                   num_workers=4)
-    for x in ['train', 'val']
-}
-dataset_sizes = {x: len(image_datasets[x]) for x in ['train', 'val']}
-class_names = train_datasets.classes
-
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-
-net = AlexNet(len(class_names))
 
 
 def evaluate_accuracy(data_iter, net, device=None):
@@ -90,13 +50,13 @@ def evaluate_accuracy(data_iter, net, device=None):
     return acc_sum / n
 
 
-def train_alexnet(net,
-                  train_iter,
-                  val_iter,
-                  optimizer,
-                  lr_scheduler,
-                  device,
-                  num_epochs=50):
+def train_rcnet(net,
+                train_iter,
+                val_iter,
+                optimizer,
+                lr_scheduler,
+                device,
+                num_epochs=50):
     since = time.time()
     net = net.to(device)
     print("Training on", device, "for", num_epochs, "epochs")
@@ -137,9 +97,46 @@ def train_alexnet(net,
     return net
 
 
-lr, num_epochs = 0.001, 50
-optimizer = torch.optim.Adam(net.parameters(), lr=lr)
-lr_scheduler = torch.optim.lr_scheduler.MultiStepLR(optimizer, [20], gamma=0.1)
-net = train_alexnet(net, dataloaders['train'], dataloaders['val'], optimizer,
+def main():
+    batch_size = 256
+    data_transforms = transforms.Compose([
+        transforms.CenterCrop(300),
+        transforms.Resize((24, 24)),
+        transforms.ToTensor(),
+        transforms.Normalize([0.44087456, 0.39025736, 0.43862119],
+                            [0.18242574, 0.19140723, 0.18536106])
+    ])
+    train_dir = 'Datasets/multiple_animals/train'
+
+    train_datasets = datasets.ImageFolder(train_dir, data_transforms)
+    print("Dataset len:", len(train_datasets))
+    train_db, val_db = torch.utils.data.random_split(train_datasets, [
+        round(len(train_datasets) * 0.8),
+        (len(train_datasets) - round(len(train_datasets) * 0.8))
+    ])
+    image_datasets = {'train': train_db, 'val': val_db}
+
+    dataloaders = {
+        x: torch.utils.data.DataLoader(image_datasets[x],
+                                    batch_size=batch_size,
+                                    shuffle=True,
+                                    num_workers=4)
+        for x in ['train', 'val']
+    }
+    class_names = train_datasets.classes
+
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+    net = RCNet(len(class_names))
+
+    lr, num_epochs = 0.001, 50
+    optimizer = torch.optim.Adam(net.parameters(), lr=lr)
+    lr_scheduler = torch.optim.lr_scheduler.MultiStepLR(optimizer, [20], gamma=0.1)
+
+    net = train_rcnet(net, dataloaders['train'], dataloaders['val'], optimizer,
                     lr_scheduler, device, num_epochs)
-torch.save(net, 'cnn_wts_reduce.pth')
+    torch.save(net, 'cnn_wts_reduce.pth')
+
+
+if __name__ == '__main__':
+    main()
