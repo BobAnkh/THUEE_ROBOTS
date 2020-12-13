fe7af94328962dc2567934b2dbb6670a16c12210
BobAnkh <bobankhshen@gmail.com>
2020-12-13 10:03:38 +0800

    fix(path_finding): change to dijkstra

--- final/Path.py
+++ final/Path.py
@@ -4,7 +4,7 @@
 # @Author       : BobAnkh
 # @Github       : https://github.com/BobAnkh
 # @Date         : 2020-12-09 19:33:06
-# @LastEditTime : 2020-12-09 20:19:53
+# @LastEditTime : 2020-12-13 10:02:50
 # @Description  : 
 
 import utils
@@ -34,6 +34,47 @@ def obs_position(number):
     return obs_pos
 
 
+def djkstra(graph, start, end):
+    path_set = set()    # 已求的路径集合
+    priority_dic = {}
+    for k in graph.keys():
+        priority_dic[k] = [9999, False, ""] # 权重表构建为一个3维数组，分别是：最小路径代价，是否计算过最小边，最小路径
+    priority_dic[start][0] = 0
+
+    # 判断权重表中所有路点是否添加完毕
+    def isSelectAll():
+        ret = True
+        for val in priority_dic.values():
+            if not val[1]:
+                ret = False
+                break
+        return ret
+
+    while not isSelectAll():
+        find_point = start
+        find_path = start
+        min_distance = 9999
+        for path in path_set:
+            end_point = path[-2:]
+            path_distance = priority_dic[end_point][0]
+            if path_distance < min_distance and not priority_dic[end_point][1]:
+                find_point = end_point
+                find_path = path
+                min_distance = path_distance
+        find_distance = priority_dic[find_point][0]
+        neighbors = graph[find_point]
+        for k in neighbors.keys():
+            p = find_path + "-" + k
+            weight = find_distance + neighbors[k]
+            path_set.add(p)
+            if weight < priority_dic[k][0]:
+                priority_dic[k][0] = weight
+                priority_dic[k][2] = p
+        priority_dic[find_point][1] = True
+
+    return priority_dic[end]
+
+
 def path_finding(start_point, end_point, barrier):
     # start_point = utils.get_location()
     # end_point = [2, 5]
@@ -59,8 +100,8 @@ def path_finding(start_point, end_point, barrier):
         ]
         # 遍历相邻坐标
         for value in Neighbour_vertices:
-            if value[0] in range(1, 6):
-                if value[1] in range(1, 6):
+            if value[0] in range(1, 10):
+                if value[1] in range(1, 10):
                     if value not in obstacle_points + path_vertices:
                         # 如果满足节点 1, 在地图边界内 2, 不在障碍物点和已经经过的点, 计算权重
                         F.append(heuristic_distace(value, end_point) + 1)
@@ -73,6 +114,36 @@ def path_finding(start_point, end_point, barrier):
         current_point = Neighbour_vertices[F.index(
             min(total_distance for total_distance in F))]
         path_vertices.append(current_point)
+    return path_vertices
+
+
+def path_finding_d(start_point, end_point, barrier):
+    if start_point == end_point:
+        return [start_point]
+    obstacle_points = [[2, 2], [2, 5], [2, 8], [5, 2], [5, 5], [5, 8], [8, 2],
+                       [8, 5], [8, 8]]
+    obstacle_points.extend(obs_position(barrier))
+    ################################################################
+    graph = {}
+    for i in range(1, 10):
+        for j in range(1, 10):
+            if [i, j] in obstacle_points:
+                continue
+            neighbor_weigh = {}
+            neighbors = [[i - 1, j], [i, j + 1], [i + 1, j], [i, j - 1]]
+            for neighbor in neighbors:
+                if neighbor[0] < 1 or neighbor[0] > 9 or neighbor[1] < 1 or neighbor[1] > 9:
+                    continue
+                elif neighbor in obstacle_points:
+                    continue
+                else:
+                    neighbor_weigh[str(neighbor[0])+str(neighbor[1])] = 1
+            graph[str(i)+str(j)] = neighbor_weigh
+    result = djkstra(graph, str(start_point[0])+str(start_point[1]), str(end_point[0])+str(end_point[1]))
+    tmp = result[2].split('-')
+    path_vertices = []
+    for elem in tmp:
+        path_vertices.append([int(elem[0]), int(elem[1])])
     return path_vertices
 
 
